4187ae4505858e0fbc5965b21426d5104a8b61c6
Alvaro <edualvardo@hotmail>
2023-03-08 05:59:22 -0500

    Subir proyecto

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>¿Quién es este pókemon?</h1>
+    <PokemonPicture :pokemonId="500" :showPokemon="true" />
+
+
+    <PokemonOptions />
+</template>
+
+<script>
+import PokemonOptions from '@/components/PokemonOptions.vue';
+import PokemonPicture from '@/components/PokemonPicture.vue';
+
+import getPokemonOptions from '@/helpers/getPokemonOptions'
+
+console.log(getPokemonOptions());
+
+
+export default {
+    components: {
+        PokemonOptions,
+        PokemonPicture
+    }
+
+}
+</script>
